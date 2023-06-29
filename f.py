2f753cf2f588bb0911f7b7940dec8ea70f97097f
jamea0723 <62099033+jamea0723@users.noreply.github.com>
2023-06-29 17:59:17 +0800

    Update f.py

--- f.py
+++ f.py
@@ -1,7 +1,7 @@
 from flask import flask, jsonify
 import gdown
 
-@app.route('/run-colab')
+@app.route('/')
 def run_colab():
     gdown.download('https://colab.research.google.com/drive/1IQVPqnOrc3dIP7NrY0GPvMIGvKARbeXJ?usp=sharing', 'tradingbot.ipynb', quiet=False)
     return jsonify(message='colab notebook ran successfully')
